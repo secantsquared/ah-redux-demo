1d7d31cbc85a8964b54bfac065fa08018fb3f3a6
MosesSupposes <mosamuel98@gmail.com>
2019-03-06 04:20:44 -0800

    add main index file for the app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/**
+ * Main index file for the app
+ * Connects the whole application to the redux store and renders it
+ */
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ 
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/presentational/App'
+import _initialState from './initialState.json'
+import { Provider } from 'react-redux'
+// import { createStore } from 'redux'
+import storeFactory from './store'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const initialState = JSON.parse(localStorage.getItem('todo-list\'s-redux-store')) || _initialState
+const store = storeFactory(initialState)
+
+function saveState() {
+	const state = JSON.stringify(store.getState())
+	localStorage.setItem('todo-list\'s-redux-store', state)
+}
+
+store.subscribe(saveState)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>, 
+    document.getElementById('root')
+)
+
