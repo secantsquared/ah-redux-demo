f99d1f8cbfd24b76ffc01b8019050473e7a9374d
Ryan Boris <ryan.boris@gmail.com>
2019-03-07 02:17:04 -0500

    adds uuid and accounts for combined reducers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import App from './components/container/App'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import reducer from './reducers/reducer'
+import rootReducer from './reducers/rootReducer'
 
 ReactDOM.render(
   <Provider
     store={createStore(
-      reducer,
+      rootReducer,
       window.__REDUX_DEVTOOLS_EXTENSION__ &&
         window.__REDUX_DEVTOOLS_EXTENSION__()
     )}
