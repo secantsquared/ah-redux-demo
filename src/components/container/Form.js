c852f69dee5ee2bcd6306610d5e7bda9d9cff2bd
MosesSupposes <mosamuel98@gmail.com>
2019-03-11 18:16:31 -0700

    style Form component

--- src/components/container/Form.js
+++ src/components/container/Form.js
@@ -2,29 +2,54 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../../actions/action'
 
+const styles = {
+  div: {
+  },
+
+  input: {
+      fontWeight: 'bold',
+      padding: '.5em',
+      paddingLeft: '.75em',
+      boxShadow: '2px 1px 2px rgba(25,25,25,0.5)',
+      border: 'none',
+  },
+  button: {
+      background: '#5cb85c',
+      border: 'none',
+      padding: '0.5em',
+      width: '3.5rem',
+      boxShadow: '2px 1px 2px rgba(25,25,25,0.6)',
+      fontSize: '.75rem',
+      marginLeft: '.10em'
+  }
+}
+
+
 class Form extends Component {
   state = { text: '' }
+
   addTodo = e => {
     e.preventDefault()
     this.props.addTodo(this.state.text)
     this.setState({ text: '' })
   }
 
-  changeHandler = e => {
+  handleChange = e => {
     this.setState({ text: e.target.value })
   }
 
   render() {
     return (
-      <div className="App">
+      <div style={styles.div}>
         <form onSubmit={this.addTodo}>
-          <input
-            type="text"
+          <input type="text"
             placeholder="enter text"
-            onChange={this.changeHandler}
+            onChange={this.handleChange}
             value={this.state.text}
+            style={styles.input}
+            autoFocus
           />
-          <button type="submit">Submit</button>
+          <button type="submit" style={styles.button}>➕</button>
         </form>
       </div>
     )
