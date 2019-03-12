e8321fe94691f68f25e244ff8bd6ccd0845572af
MosesSupposes <mosamuel98@gmail.com>
2019-03-11 18:16:53 -0700

    style top level App component

--- src/components/container/App.js
+++ src/components/container/App.js
@@ -3,11 +3,23 @@ import Form from './Form'
 import List from '../presentational/List'
 import { connect } from 'react-redux'
 
+const styles = {
+  div: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '75%', 
+    margin: '3rem auto',
+    padding: '.75rem',
+    height: '75vh',
+  }
+}
+
 const App = props => (
-  <>
+  <div style={styles.div}>
     <Form />
     <List todos={props.todos} />
-  </>
+  </div>
 )
 
 export default connect(
