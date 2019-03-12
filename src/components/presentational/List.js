c766697169bfb64107b91cb6b92448f1547275b9
MosesSupposes <mosamuel98@gmail.com>
2019-03-11 18:16:12 -0700

    style List component

--- src/components/presentational/List.js
+++ src/components/presentational/List.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
+const styles = {
+  p: {
+    textShadow: '1px 1px rgba(125,125,125,0.3)'
+  }
+}
+
 const List = props => {
   return props.todos.map(item => (
-    <p key={item.id}>{item.text}</p>
+    <p key={item.id} style={styles.p}>{item.text}</p>
   ))
 }
 
